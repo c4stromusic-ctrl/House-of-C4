9e1de84e979a71467ad1442ef368f32768514b5b
c4stromusic-ctrl <c4stromusic@gmail.com>
2026-09-15 15:09:12 +0100

    Add files via upload

---
+++ files (2)/sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'priority-app-v1';
+const urlsToCache = ['./', './index.html', './manifest.json'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache).catch(() => {})));
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      if (response) return response;
+      return fetch(event.request).then(response => {
+        if (!response || response.status !== 200) return response;
+        const responseToCache = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, responseToCache));
+        return response;
+      });
+    }).catch(() => caches.match('./index.html'))
+  );
+});
